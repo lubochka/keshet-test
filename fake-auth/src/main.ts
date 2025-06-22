9289c6f3104e2cd64575f9f8d8444cf5936629bc
Luba Karpenko <luba.ocs@gmail.com>
2025-06-22 10:38:05 +0300

    enable cors for local testing

--- fake-auth/src/main.ts
+++ fake-auth/src/main.ts
@@ -12,6 +12,10 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+   app.enableCors({
+    origin: '*', // 🔒 Use specific origin in production, e.g., 'http://localhost:3006'
+    credentials: true,
+  });
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
